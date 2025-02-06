cdef5738e2f1fb21e82e92f1c1d6843d068e665f
Ron <68036870+ronaldrespinoza@users.noreply.github.com>
2025-02-06 11:55:06 -0700

    fixed errors with filtering table not working properly

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
+import pandas as pd
 from dash import dcc, html, Input, Output, State
 from layouts.common_callbacks import common_callbacks
 from layouts.filtering_table import filtering_table_callbacks, filtering_table_layout
@@ -30,17 +31,19 @@ find_top_10_callbacks(app)
 
 # Callback that runs on application execution
 @app.callback(
-    Output("proclamation_results_filtering_table", "data"),
+    Output("proclamation_results", "data"),
     Input('interval-component', 'n_intervals'),
     State('proclamation_results', 'data')  # Check if data is already in the store
 )
 def on_page_load(n, existing_data):
+    # Debugging log
+    print(f"on_page_load triggered, n_intervals: {n}, existing_data: {existing_data}")
 
     if n == 1:  # The first interval (immediately after the page loads)
         if not existing_data:  # If no data is present in store
             deer_proclamation_df = scrape_for_deer()
             elk_proclamation_df = scrape_for_elk()
-            return deer_proclamation_df.append(elk_proclamation_df, ignore_index=False, sort=False).to_dict("dict")
+            return pd.concat([deer_proclamation_df, elk_proclamation_df], ignore_index=True, sort=False).to_dict("dict")
         return existing_data
     return existing_data  # Return existing data if n_intervals isn't 1
 
@@ -48,18 +51,17 @@ def on_page_load(n, existing_data):
 # Page layout for routing
 @app.callback(
     Output('page-content', 'children'),
-    [Input('url', 'pathname')]
+    Input('url', 'pathname')
 )
 def display_page(pathname):
+
     if pathname == '/filtering_table':
-        return filtering_table_layout  # Return the filtering table layout
+        layout = filtering_table_layout  # Return the filtering table layout
     elif pathname == '/find_top_10':
-        return find_top_10_layout  # Return the "find top 10" layout
+        layout =  find_top_10_layout  # Return the "find top 10" layout
     else:
-        return filtering_table_layout  # Return the filtering table layout
-        # return html.Div([
-        #     html.H3('404 Page Not Found')
-        # ])
+        layout =  filtering_table_layout  # Return the filtering table layout
+    return layout
 
 # Run the app
 if __name__ == '__main__':
